9cb9ef5b418f4340bc1a384cc0a8451f39105168
Your Name <you@example.com>
2020-07-23 18:35:46 +0800

    vue开发环境初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <span class="icon-back"></span>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html') // 获取根元素
+  let fontSize = window.innerWidth / 10 // 设置尺寸
+  fontSize = fontSize > 50 ? 50 : fontSize // 如果超过上限50通通计为50
+  html.style.fontSize = fontSize + 'px' // 把属性值写到根元素中
+})
+</script>
+
+<style>
+
+</style>
